07348cf6124c9dce80380a58b0408fbcb324bb02
Moosa Ahmed <moosahmed14@gmail.com>
2018-07-29 17:06:16 -0700

    provisioned public and private subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,18 @@ module "igw_network" {
 
 module "route_table_network" {
   source = "./modules/network/route_table/"
-  
+
   vpc_id = "${module.vpc_network.vpc_id}"
   igw_id = "${module.igw_network.igw_id}"
+}
+
+module "subnet_network" {
+  source = "./modules/network/subnet/"
+  
+  vpc_id = "${module.vpc_network.vpc_id}"
+  vpc_cidr_prefix = "${module.vpc_network.vpc_cidr_prefix}"
+  aws_region = "${var.aws_region}"
+
+  public_rt_id = "${module.route_table_network.public_rt_id}"
+  private_rt_id = "${module.route_table_network.private_rt_id}"
 }
---
+++ terraform/modules/network/subnet/vars.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+  description = "AWS Region"
+}
+
+## VPC Variables
+variable "vpc_id" {
+  description = "ID of the VPC to create subnet"
+}
+
+variable "vpc_cidr_prefix" {
+  description = "First 2 section of the VPC Cidr to create "
+}
+
+## Route Table
+variable "public_rt_id" {
+  description = "ID of the Public Route Table"
+}
+variable "private_rt_id" {
+  description = "ID of the Private Route Table"
+}
