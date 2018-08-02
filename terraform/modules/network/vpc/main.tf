9a377ad1b70c036667b4c2b3341184588f9e29cc
Moosa Ahmed <moosahmed14@gmail.com>
2018-08-01 22:06:15 -0700

    edited vpc provisioning to add dhcp options and associations

--- terraform/modules/network/vpc/main.tf
+++ terraform/modules/network/vpc/main.tf
@@ -1,9 +1,29 @@
-resource "aws_vpc" "theCity" {
-  cidr_block = "${lookup(var.vpc_cidr_prefix, terraform.workspace)}.${lookup(var.vpc_cidr_suffix, terraform.workspace)}"
+resource "aws_vpc" "symph-cluster-k8s-local" {
+  cidr_block           = "${lookup(var.vpc_cidr_prefix, terraform.workspace)}.${lookup(var.vpc_cidr_suffix, terraform.workspace)}"
   enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags {
-    Name = "${terraform.workspace}-vpc"
-    Environment = "${terraform.workspace}"
+    KubernetesCluster = "symph-cluster.k8s.local"
+    Name              = "${terraform.workspace}-vpc"
+    Environment       = "${terraform.workspace}"
+    "kubernetes.io/cluster/symph-cluster.k8s.local" = "owned"
   }
+}
+
+resource "aws_vpc_dhcp_options" "symph-cluster-k8s-local" {
+  domain_name         = "ec2.internal"
+  domain_name_servers = ["AmazonProvidedDNS"]
+
+  tags = {
+    KubernetesCluster = "symph-cluster.k8s.local"
+    Name              = "${terraform.workspace}-vpc-dhcp-ops"
+    Environment       = "${terraform.workspace}"
+    "kubernetes.io/cluster/symph-cluster.k8s.local" = "owned"
+  }
+}
+
+resource "aws_vpc_dhcp_options_association" "symph-cluster-k8s-local" {
+  vpc_id          = "${aws_vpc.symph-cluster-k8s-local.id}"
+  dhcp_options_id = "${aws_vpc_dhcp_options.symph-cluster-k8s-local.id}"
 }
